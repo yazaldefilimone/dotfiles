272f7e3d66d8ce4c8cc0d132a2e6f4d1e02de874
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-19 16:47:43 +0200

    *

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
+require("config.keymaps")
 require("config.options")
 require("config.autocmds")
-require("plugins")
+require("config.abbreviations")
+require("config.lazy")
+
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
