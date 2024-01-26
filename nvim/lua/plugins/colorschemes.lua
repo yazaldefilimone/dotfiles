0a093d4e1c3ee2a79a5c454c5edf70df91d63d2d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-26 14:35:27 +0200

    *

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -35,9 +35,7 @@ return {
             surface2 = "#4B4F51",
             surface1 = "#2A2D2E",
             surface0 = "#232728",
-            base = "#0e1010",
             base = "#1D2021",
-            -- base = "#0E1419", -- dark color
             mantle = "#191C1D",
             crust = "#151819",
           },
@@ -66,18 +64,18 @@ return {
             CursorLineNr = { fg = colors.surface2 },
             Pmenu = { bg = colors.crust, fg = "" },
             PmenuSel = { bg = colors.surface0, fg = "" },
-            TelescopeSelection = { bg = colors.surface0 },
-            TelescopePromptCounter = { fg = colors.mauve },
-            TelescopePromptPrefix = { bg = colors.surface0 },
-            TelescopePromptNormal = { bg = colors.surface0 },
-            TelescopeResultsNormal = { bg = colors.mantle },
-            TelescopePreviewNormal = { bg = colors.crust },
-            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-            TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
-            TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
-            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
-            TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
+            -- TelescopeSelection = { bg = colors.surface0 },
+            -- TelescopePromptCounter = { fg = colors.mauve },
+            -- TelescopePromptPrefix = { bg = colors.surface0 },
+            -- TelescopePromptNormal = { bg = colors.surface0 },
+            -- TelescopeResultsNormal = { bg = colors.mantle },
+            -- TelescopePreviewNormal = { bg = colors.crust },
+            -- TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+            -- TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+            -- TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+            -- TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
+            -- TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
+            -- TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
             IndentBlanklineChar = { fg = colors.surface0 },
             IndentBlanklineContextChar = { fg = colors.surface2 },
             GitSignsChange = { fg = colors.peach },
@@ -98,143 +96,10 @@ return {
     enabled = true,
     priority = 1000,
     config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_material_background = "hard"
       -- vim.g.gruvbox_material_transparent_background = 1
-      vim.g.gruvbox_material_foreground = 'material'
-      vim.g.gruvbox_material_background = 'hard'  -- soft, medium, hard
-      vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
-      vim.g.gruvbox_material_float_style = 'dim'  -- Background of floating windows
-      vim.g.gruvbox_material_statusline_style = 'material'
-
-      -- if (vim.o.background == 'light') then
-      --   vim.g.gruvbox_material_colors_override = { bg0 = '#f9f5d7' }
-      -- else
-      --   vim.g.gruvbox_material_colors_override = { bg0 = '#0e1010' }
-      -- end
-
-      vim.g.gruvbox_material_colors_override = { bg0 = '#0e1010' } -- #0e1010
-      vim.g.gruvbox_material_better_performance = 1
       vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
-  {
-    "rose-pine/neovim",
-    config = function()
-      require('rose-pine').setup({
-        disable_background = true,
-        disable_float_background = true,
-        dark_variant = 'moon',
-        disable_italics = true,
-        groups = {
-          background = 'base',
-          background_nc = '_experimental_nc',
-          panel = 'surface',
-          panel_nc = 'base',
-          border = 'highlight_med',
-          comment = 'muted',
-          link = 'iris',
-          punctuation = 'subtle',
-
-          error = 'love',
-          hint = 'iris',
-          info = 'foam',
-          warn = 'gold',
-
-          headings = {
-            h1 = 'iris',
-            h2 = 'foam',
-            h3 = 'rose',
-            h4 = 'gold',
-            h5 = 'pine',
-            h6 = 'foam',
-          }
-        },
-        highlight_groups = {
-          ColorColumn = { bg = 'rose' },
-
-          -- Blend colours against the "base" background
-          CursorLine = { bg = 'foam', blend = 10 },
-          StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-          Search = { bg = 'gold', inherit = false },
-
-          TelescopeBorder = { fg = "highlight_high", bg = "none" },
-          TelescopeNormal = { bg = "none" },
-          TelescopePromptNormal = { bg = "base" },
-          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-          TelescopeSelection = { fg = "text", bg = "base" },
-          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-        },
-      })
-
-      -- vim.cmd.colorscheme 'rose-pine-moon'
-    end
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    enabled = true,
-    config = function()
-      require("kanagawa").setup({
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          }
-        end,
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none"
-              }
-            }
-          }
-        }
-      })
-      -- vim.cmd("colorscheme kanagawa-wave")
-    end
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000 -- Ensure it loads first
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = false,
-        underline = false,
-        bold = false,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = false,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true,    -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = true,
-      })
-      -- vim.cmd.colorscheme("gruvbox")
-    end
-  }
 }
