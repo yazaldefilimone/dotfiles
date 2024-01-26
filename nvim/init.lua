7e0ae30d97ccc2e55240feb3aee8d5ad7cad1700
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-26 14:35:27 +0200

    *

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,4 @@
 require("config.keymaps")
 require("config.options")
 require("config.autocmds")
-require("config.abbreviations")
 require("config.lazy")
-
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
