cd797b00d050f19a2309e46709df9231a9f20bf2
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-26 14:35:27 +0200

    *

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -2,8 +2,6 @@ return {
   "j-hui/fidget.nvim",
   branch = "legacy",
   config = function()
-    require('fidget').setup({
-      window = { blend = 0 },
-    })
+    require('fidget').setup()
   end
 }
