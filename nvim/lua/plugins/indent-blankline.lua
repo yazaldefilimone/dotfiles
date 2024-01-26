75974824445be6f4bbe5b6f128bcaf809ebf5f4e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-26 14:35:27 +0200

    *

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,10 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  enabled = false,
-  version = "2.1.0",
+  enabled = true,
   opts = {
     -- char = "▏",
-    char = "┊",
+    char = '┊',
     -- char = "│",
     filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
     show_trailing_blankline_indent = false,
