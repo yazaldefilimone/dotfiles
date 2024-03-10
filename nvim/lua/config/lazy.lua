63d9096af896ec06ba4bc0209616bb4a3f275781
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-10 16:00:43 +0200

    *

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,9 +12,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
+require("lazy").setup({import = "plugins"}, {
   install = {
     missing = true,
+    -- colorscheme = { "catppuccin", "habamax" }
   },
   checker = {
     enabled = true,
@@ -23,6 +24,9 @@ require("lazy").setup("plugins", {
   change_detection = {
     enabled = true,
     notify = false,
+  },
+  ui = {
+    border = "rounded"
   },
   performance = {
     rtp = {
