a3a7e413512d41b00b4222561ae73cb4a0176b8f
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-10 16:00:43 +0200

    *

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,9 +1,7 @@
 return {
     "jose-elias-alvarez/null-ls.nvim",
     lazy = false,
-
     opts = function(_, opts)
-        -- table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
-    end,
+    end
     -- TODO
 }
