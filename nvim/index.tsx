a79aeb6393e8024eae5020b801278ff2a057ba11
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-10 16:00:43 +0200

    *

---
+++ nvim/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+export const name = "hello";
+
+
+
+
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+
+
+
+
+
+
+
+
+
+
+      <h1>{name}</h1>
+    </div>
+  )
+}
