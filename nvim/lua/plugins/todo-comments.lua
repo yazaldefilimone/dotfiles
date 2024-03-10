502372d31c922d8da29cd1d767771d3f4a66016f
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-10 16:00:43 +0200

    *

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -8,29 +8,36 @@ return {
         require("todo-comments").setup({
             keywords = {
                 FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+                fix = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
                 TODO = { icon = " ", color = "info" },
+                todo = { icon = " ", color = "info" },
                 HACK = { icon = " ", color = "warning" },
+                hack = { icon = " ", color = "warning" },
                 WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                warn = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
                 PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                perf = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
                 NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                note = { icon = " ", color = "hint", alt = { "INFO" } },
                 TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+                test = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
             },
             gui_style = {
-                fg = "NONE",                         -- The gui style to use for the fg highlight group.
-                bg = "BOLD",                         -- The gui style to use for the bg highlight group.
+                fg = "NONE",                     -- The gui style to use for the fg highlight group.
+                bg = "BOLD",                     -- The gui style to use for the bg highlight group.
             },
-            merge_keywords = true,                   -- when true, custom keywords will be merged with the defaults
+            merge_keywords = true,               -- when true, custom keywords will be merged with the defaults
             highlight = {
-                multiline = true,                    -- enable multine todo comments
-                multiline_pattern = "^.",            -- lua pattern to match the next multiline from the start of the matched keyword
-                multiline_context = 10,              -- extra lines that will be re-evaluated when changing a line
-                before = "",                         -- "fg" or "bg" or empty
-                keyword = "wide",                    -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty.
-                after = "fg",                        -- "fg" or "bg" or empty
-                pattern = [[.*<(KEYWORDS)\s*:]],     -- pattern or table of patterns, used for highlighting (vim regex)
-                comments_only = true,                -- uses treesitter to match keywords in comments only
-                max_line_len = 400,                  -- ignore lines longer than this
-                exclude = {},                        -- list of file types to exclude highlighting
+                multiline = true,                -- enable multine todo comments
+                multiline_pattern = "^.",        -- lua pattern to match the next multiline from the start of the matched keyword
+                multiline_context = 10,          -- extra lines that will be re-evaluated when changing a line
+                before = "",                     -- "fg" or "bg" or empty
+                keyword = "wide",                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty.
+                after = "fg",                    -- "fg" or "bg" or empty
+                pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+                comments_only = true,            -- uses treesitter to match keywords in comments only
+                max_line_len = 400,              -- ignore lines longer than this
+                exclude = {},                    -- list of file types to exclude highlighting
             },
             -- list of highlight groups or use the hex color if hl not found as a fallback
             colors = {
